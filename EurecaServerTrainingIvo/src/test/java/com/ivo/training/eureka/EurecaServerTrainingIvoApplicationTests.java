9a4be961202af549248cfff486a16e9bbaec0d8e
Ivo <Ivo@DESKTOP-B0034AN>
2021-01-18 10:39:10 +0200

    first commit eureka server

---
+++ EurecaServerTrainingIvo/src/test/java/com/ivo/training/eureka/EurecaServerTrainingIvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivo.training.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaServerTrainingIvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
